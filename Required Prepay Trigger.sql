d6a8bae2c4fa7b1533c7eb2cab6f9d5931c532f1
kishoo-coder <85311617+kishoo-coder@users.noreply.github.com>
2024-04-26 11:30:52 +0300

    Delete Required Prepay Trigger.sql

--- Required Prepay Trigger.sql
+++
@@ -1,8 +0,0 @@
-create or replace trigger update_required_prepay_trigger
-before insert or update ON RESERVATION for each row
-begin
-    if :new.people <= 10 then
-        :new.required_prepay := 'true';
-    end if;
-end;
-/
